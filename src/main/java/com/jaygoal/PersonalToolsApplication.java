bd026c447f365c94ff49fbd19434dd696292c4a2
JayGoal <yj936290344@163.com>
2020-08-21 15:29:36 +0800

    窗口模板

--- src/main/java/com/jaygoal/PersonalToolsApplication.java
+++ src/main/java/com/jaygoal/PersonalToolsApplication.java
@@ -1,7 +1,10 @@
 package com.jaygoal;
 
+import com.jaygoal.windows.MainWindow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 
 /**
  * @author jaygoaler
@@ -10,6 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PersonalToolsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PersonalToolsApplication.class,args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(PersonalToolsApplication.class);
+        ApplicationContext context = builder.headless(false).run(args);
+        MainWindow window = context.getBean(MainWindow.class);
+        window.setVisible(true);
     }
 }
--- src/main/java/com/jaygoal/windows/MainWindow.java
+++ src/main/java/com/jaygoal/windows/MainWindow.java
@@ -4,28 +4,89 @@
 
 package com.jaygoal.windows;
 
+import java.awt.event.*;
+import org.springframework.stereotype.Component;
+
 import java.awt.*;
 import javax.swing.*;
 
 /**
  * @author unknown
  */
+@Component
 public class MainWindow extends JFrame {
     public MainWindow() {
         initComponents();
     }
 
+    private void thisWindowClosing(WindowEvent e) {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        panel1 = new JPanel();
+        panel2 = new JPanel();
+        panel3 = new JPanel();
+        panel4 = new JPanel();
+        panel5 = new JPanel();
+        label1 = new JLabel();
 
         //======== this ========
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                thisWindowClosing(e);
+            }
+        });
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
+
+        //======== panel1 ========
+        {
+            panel1.setLayout(new BorderLayout());
+        }
+        contentPane.add(panel1, BorderLayout.NORTH);
+
+        //======== panel2 ========
+        {
+            panel2.setLayout(new BorderLayout());
+        }
+        contentPane.add(panel2, BorderLayout.WEST);
+
+        //======== panel3 ========
+        {
+            panel3.setLayout(new BorderLayout());
+        }
+        contentPane.add(panel3, BorderLayout.EAST);
+
+        //======== panel4 ========
+        {
+            panel4.setLayout(new BorderLayout());
+        }
+        contentPane.add(panel4, BorderLayout.SOUTH);
+
+        //======== panel5 ========
+        {
+            panel5.setLayout(new BorderLayout());
+
+            //---- label1 ----
+            label1.setText("Personal Tool");
+            label1.setHorizontalAlignment(SwingConstants.CENTER);
+            panel5.add(label1, BorderLayout.NORTH);
+        }
+        contentPane.add(panel5, BorderLayout.CENTER);
         pack();
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JPanel panel1;
+    private JPanel panel2;
+    private JPanel panel3;
+    private JPanel panel4;
+    private JPanel panel5;
+    private JLabel label1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
